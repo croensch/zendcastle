e2961d0f88108b3d77933323b52a9338b29b2a55
Christoph <christoph.roensch@arcor.de>
2011-07-29 00:42:25 +0200

    *FIX* public/index.php

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$count_declared_classes = count(get_declared_classes());
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
+            
+?><pre><?php
+foreach( array_slice(get_declared_classes(), $count_declared_classes) as $i => $class ){
+	echo "\t[$i] $class\n";
+}
+?></pre>
