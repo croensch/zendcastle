1c9e6835d00612fe7a4d4a01b0b61c28b1d52d13
croensch <christoph.roensch@arcor.de>
2015-05-12 16:56:41 +0200

    *ADD* proove that date objects can have a cache with our resource

--- application/controllers/ZcController.php
+++ application/controllers/ZcController.php
@@ -13,6 +13,16 @@ class ZcController extends Zend_Controller_Action
     	
     	// cachemanager+
     	// date+
+		$ZendDateHasCache = false;
+		$rDateObject = new ReflectionClass('Zend_Date_DateObject');
+		$rProperties = $rDateObject->getProperties(ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_STATIC);
+		foreach ($rProperties as $rProperty) {
+			if ($rProperty->name === '_cache') {
+				$rProperty->setAccessible(true);
+				$ZendDateHasCache = ($rProperty->getValue() instanceof Zend_Cache_Core);
+			}
+		}
+		$this->view->ZendDateHasCache = $ZendDateHasCache ? 'yep' : 'nope';
     	// db+
     	$this->view->ZendDbAdapter = get_class($bootstrap->getResource('Db'));
     	$this->view->ZendDbTableHasDefaultMetadataCache = (Zend_Db_Table::getDefaultMetadataCache() instanceof Zend_Cache_Core ? 'yep' : 'nope');
